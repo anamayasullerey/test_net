9d41629fad800110062d795b63a8c7680bbff8b9
ANamaya Sullerey <anamaya@gmail.com>
2017-11-30 11:24:58 -0500

    Changes to the mnist test.

--- src/mnist_digits.py
+++ src/mnist_digits.py
@@ -1,39 +1,96 @@
 import numpy as np
 import neural_network.neural_network as nn
+import neural_network.layer_types as lt
+import neural_network.loss_layer as ll
 import neural_network.weight_update_params as wup
 import utils.load_mnist as load_mnist
 import random
 
+def show(image):
+    """
+    Render a given numpy.uint8 2D array of pixel data.
+    """
+    from matplotlib import pyplot
+    import matplotlib as mpl
+    fig = pyplot.figure()
+    ax = fig.add_subplot(1,1,1)
+    imgplot = ax.imshow(image, cmap=mpl.cm.Greys)
+    imgplot.set_interpolation('nearest')
+    ax.xaxis.set_ticks_position('top')
+    ax.yaxis.set_ticks_position('left')
+    pyplot.show()
+
+def validate(validation_data, net):
+  validation_results = [(net.predict_classify(x) == y) for x, y in validation_data]
+  return sum(result for result in validation_results)
+    
 training_data_zip, validation_data_zip, test_data_zip = load_mnist.load_mnist()
+#print(type(training_data_zip))
 training_data = list(training_data_zip)
 validation_data = list(validation_data_zip)
 test_data = list(test_data_zip)
 net = nn.NeuralNetwork("test_net", 784)
-net.add_layer(40, "relu")
-net.add_layer(10, "sigmoid")
-net.set_loss_function("sigmoid_cross_entropy_loss", 0)
+layer = lt.ldict["fc"](800)
+net.add_layer(layer)
+layer = lt.ldict["relu"](800)
+net.add_layer(layer)
+layer = lt.ldict["fc"](80)
+net.add_layer(layer)
+layer = lt.ldict["relu"](80)
+net.add_layer(layer)
+layer = lt.ldict["fc"](10)
+net.add_layer(layer)
+layer = lt.ldict["sigmoid"](10)
+net.add_layer(layer)
+layer = ll.LossLayer("sigmoid_cross_entropy_loss")
+net.add_layer(layer)
+net.set_l2_loss_coeff(.00)
 
 np.random.seed(1)
-params = wup.GradientDescentParams()
+params = wup.GradientDescentParams(.3)
+#params = wup.AdamParams()
+#params = wup.MomentumParams()
 net.set_weight_update_function(params)
 net.initialize_parameters()
 
-mini_batch_size = 2
 # random.shuffle(training_data)
-for epoch in range(10):
-  random.shuffle(training_data)
-  mini_batch = [training_data[k:k + mini_batch_size] for k in range(0, len(training_data), mini_batch_size)]
-  print(mini_batch)
-  for x, y in mini_batch:
-    net.train(x, y)            
+print ("Training set size is " + str(len(training_data)))
+mini_batch_size = 32
+epochs = 1
+print("Training data shape: ")
+for epoch in range(epochs):
+  print(" Epoch " + str(epoch))
+  #random.shuffle(training_data)
+  mini_batches = [training_data[k:k + mini_batch_size] for k in
+                  range(0, len(training_data), mini_batch_size)]
+
+  for count, mini_batch in enumerate(mini_batches, start=1):
+    x = np.array(mini_batch[0][0])
+    y = np.array(mini_batch[0][1])
+    for i in range(1, len(mini_batch)):
+      x = np.append(x, mini_batch[i][0], 1)
+      y = np.append(y, mini_batch[i][1], 1)
+
+    net.train(x, y)
   
-  accuracy = validate(validation_data, net) / 100.0
-  print("Epoch {0}, accuracy {1} %.".format(epoch + 1, accuracy))
+    if (count%10 == 0):
+      print ("Processing  minibatch : " + str(count))
+      correct = validate(validation_data, net)
+      accuracy = correct / 100.0
+      print("----------------------")
+      print()
+      print ("Running epoch " + str(epoch))
+      print("Epoch {0}, accuracy {1} %.".format(epoch + 1, accuracy))
+  
+  #show(x.reshape((28,28)))           
+  #if (count%100 == 0) and (epoch != 0):
+  #  params.learning_rate = params.learning_rate/2
+  #  print("Changing learning rate to " + str(params.learning_rate))
+  #  net.set_weight_update_function(params)
 
-def predict(net, x):
-  net.forward_prop(x)  
-  return net.argmax(net.layer[-1].activations)
-
-def validate(self, validation_data, net):
-  validation_results = [(predict(net, x) == y) for x, y in validation_data]
-  return sum(result for result in validation_results)
+#
+print("Epoch {0}, accuracy {1} %.".format(epoch + 1, accuracy))
+for x, y in validation_data:
+  if (net.predict_classify(x) > 9):
+    show(x.reshape((28,28)))           
+     
