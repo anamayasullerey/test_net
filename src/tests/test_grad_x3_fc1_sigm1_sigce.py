d3eee8498366c5887651baf3d5b4df1a49e2dbdc
ANamaya Sullerey <anamaya@gmail.com>
2017-12-10 21:54:41 -0500

    Test cleanup

---
+++ src/tests/test_grad_x3_fc1_sigm1_sigce.py
@@ -0,0 +1,30 @@
+import nn_test as nt
+import numpy as np
+import nnframework.neural_network as nn
+import nnframework.layer_dict as ld
+import nnframework.weight_update_params as wup
+
+class test_grad_x3_fc1_sigm1_sigce(nt.NnTest):
+        
+    def define_nn(self):
+        self.net = nn.NeuralNetwork("test_net", 1)
+
+        self.layer = ld.hdict["fc"](1)
+        self.net.add_layer(self.layer)
+
+        self.layer = ld.hdict["sigmoid"](1)
+        self.net.add_layer(self.layer)
+
+        self.layer = ld.odict["loss"]("sigmoid_cross_entropy_loss")
+        self.net.add_layer(self.layer)
+
+        np.random.seed(1)
+
+        self.params = wup.GradientDescentParams(0)
+        self.net.set_weight_update_function(self.params)
+        self.net.initialize_parameters()
+        self.net.layers[1].weights[0,0] = 10
+    
+    def set_training_example(self):
+        self.x = np.array([[0.5]])
+        self.y = np.array([[0.5]])
---
+++ src/tests/test_grad_x3_fc4_softmaxo4.py
@@ -0,0 +1,27 @@
+import nn_test as nt
+import numpy as np
+import nnframework.neural_network as nn
+import nnframework.layer_dict as ld
+import nnframework.weight_update_params as wup
+
+class test_grad_x3_fc1_sigm1_sigce(nt.NnTest):
+        
+    def define_nn(self):
+        self.net = nn.NeuralNetwork("test_net", 3)
+
+        self.layer = ld.hdict["fc"](4)
+        self.net.add_layer(self.layer)
+
+        self.layer = ld.odict["softmax"](4)
+        self.net.add_layer(self.layer)
+
+        np.random.seed(1)
+
+        self.params = wup.GradientDescentParams(0)
+        self.net.set_weight_update_function(self.params)
+        self.net.initialize_parameters()
+        self.net.layers[1].weights[0,0] = 10
+    
+    def set_training_example(self):
+        self.x = np.array([[1], [2], [3]])
+        self.y = np.array([[1], [0], [0], [0]])
